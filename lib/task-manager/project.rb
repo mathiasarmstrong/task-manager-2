884293fa73c4e90d9a08f2b34b4c6b7248814e14
Jered McCullough <mathias.armstrong@gmail.com>
2014-06-22 14:13:46 -0700

    I changed the projects.rb folder to include to simplify and remove an extra if

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -8,14 +8,6 @@ class TM::Project
     @id = id
   end
 
-#------task methods----------------
-
-  #   tasks = tasks.sort do |task1,task2|
-  #     comp = ( task1.priority <=> task2.priority )
-  #     comp == 0 ? ( task1.creation_date <=> task2.creation_date ) : comp
-  #   end
-  # end
-
 #--------project methods----------
 
   def self.create( name )
@@ -34,10 +26,11 @@ class TM::Project
   def self.list_incomplete_tasks( pid )
     all_tasks = TM::Task.list_by_project( pid )
     tasks = all_tasks.select{ |task| task.completed == false }
-    tasks.sort do |task1,task2|
-      comp = task1.priority<=>task2.priority
-      if comp ==0 ? task1.creation_date <=> task2.creation_date : comp
+    tasks = tasks.sort do |task1,task2|
+      comp = task1.priority <=> task2.priority
+      comp == 0 ? task1.creation_date <=> task2.creation_date : comp
     end
+    tasks
   end
 
   def self.select( pid )
@@ -45,7 +38,7 @@ class TM::Project
   end
 
   def self.add_employee ( pid, eid )
-    TM.orm.add_employee_to_project( pid, eid ) unless select( pid ).nil?
+    TM.orm.add_employee_to_project( pid, eid )
   end
 
   def self.list_by_employee( eid )
